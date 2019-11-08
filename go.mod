03087ca5a749f995c79bfb7acefefe45706feded
Akezhan <yesbolatovakezhan@2step.me>
2019-11-09 00:23:09 +0600

    simple test for hello world

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AkezhanOb1/dotaTelegramBot
+
+go 1.13
+
+require gopkg.in/tucnak/telebot.v2 v2.0.0-20191005061224-d0707a9d73c4
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	var (
+		port      = "6677"
+		publicURL = "https://dotapollbot.herokuapp.com"
+		token     = "923261617:AAGIM0wr4rrUZxFL7xCAn1i62eQK5h7pCUE"
+	)
+
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+
+	b, err := tb.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hi!")
+	})
 }
